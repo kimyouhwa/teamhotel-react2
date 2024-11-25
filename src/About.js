8accfc5a417f10ca09e37761c75dc80455615ce2
kymyouhwa <s_fan@naver.com>
2024-11-25 17:36:08 +0900

    라우팅 일차작업

--- src/About.js
+++ src/About.js
@@ -4,14 +4,14 @@ import './About.css';
 import {Link} from 'react-router-dom';
 const About= () =>  {
     return (
-        <nav className="About_container">
+        
+      <nav className="About_container">
             <h2>About</h2>
             <ul className='About_list'>
                 <li><Link to ="/Notice">Notice</Link></li>
                 <li><Link to ="/Q&A">Q&A</Link></li>
             </ul>
          </nav>
-      
     );
 };
 
